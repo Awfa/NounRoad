df6f1ae75267c9bd3a4fddae1a904ef09d70e0de
Kristy Bui <bui.kristy@gmail.com>
2015-02-14 15:14:53 -0800

    Make WordManager class Manages the word play in the game

--- core/src/com/awfa/nounroad/WordManager.java
+++ core/src/com/awfa/nounroad/WordManager.java
@@ -3,19 +3,55 @@ package com.awfa.nounroad;
 import java.util.*;
 
 public class WordManager {
+	
+	private Set<String> usedWords;
+	private Set<String> availableWords;
+	private Deque<String> recentUsed;
+	
+	public static final int MAX_RECENT_WORDS = 10;
+	
 	public WordManager(Set<String> words) {
-		
+		availableWords = words;
+		usedWords = new HashSet<String>();
+		recentUsed = new LinkedList<String>();
 	}
 	
 	public boolean playWord(String givenWord) {
-		return true;
+		givenWord = givenWord.toLowerCase().trim();
+		
+		if(usedWords.contains(givenWord)) {
+			return false;
+		} else if(!availableWords.contains(givenWord)) {
+			return false;
+		} else if(recentUsed.isEmpty()){
+			usedWords.add(givenWord);
+			recentUsed.addFirst(givenWord);
+			return true;
+		} else {
+			String compareWord = recentUsed.peekFirst();
+			Character c = compareWord.charAt(compareWord.length() - 1);
+			
+			if(givenWord.charAt(0) == c) {
+				usedWords.add(givenWord);
+				recentUsed.addFirst(givenWord);
+				if(recentUsed.size() > MAX_RECENT_WORDS) {
+					recentUsed.removeLast();
+				}
+				
+				return true;
+			} else {
+				return false;
+			}
+		}
 	}
 	
 	public Collection<String> getUsedWords() {
-		return new ArrayList<String>();
+		return recentUsed;
 	}
 	
 	public void resetGame() {
-		
+		usedWords.clear();
+		recentUsed.clear();
 	}
 }
+
